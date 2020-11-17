a51f4d40cf64ab9125cf85bfd984dbcdd038d2ce
RandomityGuy <krishiv768@yahoo.co.in>
2020-11-17 22:11:07 +0530

    assignments start

--- ChatAppTest/api/permissions.py
+++ ChatAppTest/api/permissions.py
@@ -2,6 +2,7 @@ class Permissions:
     STUDENT = 0
     MANAGE_ANNOUNCE = 1
     CAN_MODIFY_AVATAR = 2
+    CAN_MODIFY_STUDENT = 4
 
     @staticmethod
     def has_permission(flags: int, permission: int) -> bool:

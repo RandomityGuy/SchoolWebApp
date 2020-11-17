7e059769d042b84715d45cf9b4759bc566d4eab3
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2020-11-17 21:10:18 +0530

    Format the code

--- ChatAppTest/api/announcements.py
+++ ChatAppTest/api/announcements.py
@@ -1,59 +1,60 @@
-from api.base import *;
-from api.auth import Auth;
-from api.permissions import Permissions;
+from api.base import *
+from api.auth import Auth
+from api.permissions import Permissions
+
 
 class Announcement(ToDictable):
-    def __init__(self,id,creator,byclass,content):
-        self.id = id;
-        self.creator = creator;
-        self.byclass = byclass;
-        self.content = content;
+    def __init__(self, id, creator, byclass, content):
+        self.id = id
+        self.creator = creator
+        self.byclass = byclass
+        self.content = content
 
     def ToDict(self):
-        return {"id":self.id, "creator":self.creator, "class": self.byclass, "content": self.content};
+        return {"id": self.id, "creator": self.creator, "class": self.byclass, "content": self.content}
+
 
 class Announcements:
-
-    @staticmethod 
-    def make_announcement(user,destclass,text):
-        perms = Auth.get_permissions(user);
-        if (Permissions.has_permission(perms,Permissions.MANAGE_ANNOUNCE)):
-            id = snowflakegen.__next__();
-            cursor.execute("INSERT INTO announcements VALUES(%s,%s,%s,%s);",(id,user,destclass,text));
-            db.commit();
-            return id;
+    @staticmethod
+    def make_announcement(user: int, destclass: str, text: str) -> int:
+        perms = Auth.get_permissions(user)
+        if Permissions.has_permission(perms, Permissions.MANAGE_ANNOUNCE):
+            id = snowflakegen.__next__()
+            cursor.execute("INSERT INTO announcements VALUES(%s,%s,%s,%s);", (id, user, destclass, text))
+            db.commit()
+            return id
         else:
-            return False;
+            return False
 
     @staticmethod
-    def revoke_announcement(user,announcementid):
-        perms = Auth.get_permissions(user);
-        if (Permissions.has_permission(perms,Permissions.MANAGE_ANNOUNCE)):
-            id = snowflakegen.__next__();
-            cursor.execute("DELETE FROM announcements WHERE id = %s;",(announcementid,));
-            db.commit();
-            return True;
+    def revoke_announcement(user: int, announcementid: int) -> bool:
+        perms = Auth.get_permissions(user)
+        if Permissions.has_permission(perms, Permissions.MANAGE_ANNOUNCE):
+            id = snowflakegen.__next__()
+            cursor.execute("DELETE FROM announcements WHERE id = %s;", (announcementid,))
+            db.commit()
+            return True
         else:
-            return False;
+            return False
 
     @staticmethod
-    def get_announcements_by_user(user):
+    def get_announcements_by_user(user: int) -> list[Announcement]:
 
-        cursor.execute("SELECT a.id,a.byuser,a.class,a.content FROM announcements as a,chatusers WHERE chatusers.class = a.class && chatusers.id = %s;",(user,));
-        res = cursor.fetchall();
-        L = [];
-        for (id,byuser,clas,content) in res:
-            L.append(Announcement(id,byuser,clas,content));
+        cursor.execute("SELECT a.id,a.byuser,a.class,a.content FROM announcements as a,chatusers WHERE chatusers.class = a.class && chatusers.id = %s;", (user,))
+        res = cursor.fetchall()
+        L = []
+        for (id, byuser, clas, content) in res:
+            L.append(Announcement(id, byuser, clas, content))
 
-        return L;
+        return L
 
     @staticmethod
-    def get_announcements_by_class(userclass):
+    def get_announcements_by_class(userclass: str) -> list[Announcement]:
 
-        res = cursor.execute("SELECT a.id,a.byuser,a.class,a.content FROM announcements as a,chatusers WHERE a.class = %s;",(userclass,));
+        res = cursor.execute("SELECT a.id,a.byuser,a.class,a.content FROM announcements as a,chatusers WHERE a.class = %s;", (userclass,))
 
-        L = [];
-        for (id,byuser,clas,content) in res:
-            L.append(Announcement(id,byuser,clas,content));
+        L = []
+        for (id, byuser, clas, content) in res:
+            L.append(Announcement(id, byuser, clas, content))
 
-        return L;
+        return L
--- ChatAppTest/api/auth.py
+++ ChatAppTest/api/auth.py
@@ -1,105 +1,103 @@
-from api.base import *;
-import bcrypt;
-import secrets;
-import hashlib;
-import base64;
+from api.base import *
+import bcrypt
+import secrets
+import hashlib
+import base64
+
 
 class Auth:
+    @staticmethod
+    def login(username: str, pwd: str) -> str:
+        """
+        Attempt login as given username and password, returns token if success
 
+        @param username: given username
+        @param pwd: the password
+        @return: token if success
 
-	@staticmethod
-	def login(username,pwd):
-		'''
-		Attempt login as given username and password, returns token if success
-		
-		@param username: given username
-		@param pwd: the password
-		@return: token if success
+        """
 
-		'''
+        # !!! WARNING : SEND A HASHED PASSWORD FROM THE SITE, HASH THE PASSWORD WITHIN THE BROWSER AND THEN SEND IT HERE
 
-		# !!! WARNING : SEND A HASHED PASSWORD FROM THE SITE, HASH THE PASSWORD WITHIN THE BROWSER AND THEN SEND IT HERE
+        cursor.execute("SELECT id,Username,password FROM chatusers WHERE Username = %s;", username)
 
-		cursor.execute("SELECT id,Username,password FROM chatusers WHERE Username = %s;",username);
+        if cursor.rowcount == 0:
+            raise Exception("Invalid username")
 
-		if (cursor.rowcount == 0):
-			raise Exception("Invalid username");
+        data = cursor.fetchone()
 
-		data = cursor.fetchone();
+        if data[2] == None:
+            raise Exception("No password set")
 
-		if (data[2] == None):
-			raise Exception("No password set");
+        if bcrypt.checkpw(base64.b64encode(hashlib.sha256(pwd).digest()), data[2]):
+            cursor.execute("SELECT token FROM tokens WHERE (expires > CURDATE() && user=%s);", data[0])
+            if cursor.rowcount == 0:
+                # Create new token
+                token = secrets.token_hex(128)
+                id = snowflakegen.__next__()
+                cursor.execute("INSERT INTO tokens VALUES(%s,%s,DATEADD(m,1,CURDATE()),%s);", (id, token, data[1]))
+                db.commit()
+                return token
+            else:
+                token = cursor.fetchone()
+                return token
 
-		if (bcrypt.checkpw(base64.b64encode(hashlib.sha256(pwd).digest()),data[2])):
-			cursor.execute("SELECT token FROM tokens WHERE (expires > CURDATE() && user=%s);",data[0]);
-			if (cursor.rowcount == 0):
-				# Create new token
-				token = secrets.token_hex(128);
-				id = snowflakegen.__next__();
-				cursor.execute("INSERT INTO tokens VALUES(%s,%s,DATEADD(m,1,CURDATE()),%s);",(id,token,data[1]));
-				db.commit();
-				return token;
-			else:
-				token = cursor.fetchone();
-				return token;
+        raise Exception("Invalid password")
 
-		raise Exception("Invalid password");
+    @staticmethod
+    def register(username: str, pwd: str, permissions: int) -> str:
+        """
+        Register user from username,password and permissions flags. Returns token
 
-	@staticmethod
-	def register(username,pwd,permissions):
-		'''
-		Register user from username,password and permissions flags. Returns token
+        @param username: username
+        @param pwd: the password
+        @param permissions: the set of flags
+        @return: user token
 
-		@param username: username
-		@param pwd: the password
-		@param permissions: the set of flags
-		@return: user token
+        """
+        id = snowflakegen.__next__()
+        hash = bcrypt.hashpw(base64.b64encode(hashlib.sha256(pwd).digest()), bcrypt.gensalt())
 
-		'''
-		id = snowflakegen.__next__();
-		hash = bcrypt.hashpw(base64.b64encode(hashlib.sha256(pwd).digest()),bcrypt.gensalt());
+        cursor.execute("INSERT INTO chatdb VALUES(%s,%s,%s,%s);", (id, username, hash, permissions))
+        db.commit()
 
-		cursor.execute("INSERT INTO chatdb VALUES(%s,%s,%s,%s);",(id,username,hash,permissions));
-		db.commit();
+        return Auth.login(username, pwd)
 
-		return Auth.login(username,pwd);
+    @staticmethod
+    def authorize(token: str) -> bool:
+        """
+        Authorize token. Returns True if success
 
-	@staticmethod
-	def authorize(token):
-		'''
-		Authorize token. Returns True if success
+        @param token: token
+        @return: True if success
+        """
+        cursor.execute("SELECT token FROM tokens WHERE (expires > CURDATE() && token=%s);", (token,))
+        if cursor.rowcount == 0:
+            return False
+        return True
 
-		@param token: token
-		@return: True if success
-		'''
-		cursor.execute("SELECT token FROM tokens WHERE (expires > CURDATE() && token=%s);",(token,));
-		if (cursor.rowcount == 0):
-			return False;
-		return True;
+    @staticmethod
+    def get_token_permissions(token: str) -> int:
+        res = cursor.execute("SELECT permissions FROM tokens,chatusers WHERE (tokens.user = chatusers.id && token = %s);", (token,))
+        if cursor.rowcount == 0:
+            return None
+        perms = res.fetchone()
+        return perms
 
-	@staticmethod
-	def get_token_permissions(token):
-		res = cursor.execute("SELECT permissions FROM tokens,chatusers WHERE (tokens.user = chatusers.id && token = %s);",(token,));
-		if (cursor.rowcount == 0):
-			return None;
-		perms = res.fetchone();
-		return perms;
+    @staticmethod
+    def get_permissions(userid: int) -> int:
+        res = cursor.execute("SELECT permissions FROM chatusers WHERE id = %s;", userid)
+        if cursor.rowcount == 0:
+            return None
+        perms = res.fetchone()
+        return perms
 
-	@staticmethod
-	def get_permissions(userid):
-		res = cursor.execute("SELECT permissions FROM chatusers WHERE id = %s;",userid);
-		if (cursor.rowcount == 0):
-			return None;
-		perms = res.fetchone();
-		return perms;
-
-
-	@staticmethod
-	def get_token_user_id(token):
-		if (Auth.authorize(token)):
-			query = "SELECT chatusers.id FROM tokens,chatusers WHERE tokens.user=chatusers.id;";
-			cursor.execute(query);
-			if (cursor.rowcount == 0):
-				return None;
-			return cursor.fetchone()[0];
-		return None;
+    @staticmethod
+    def get_token_user_id(token: str) -> int:
+        if Auth.authorize(token):
+            query = "SELECT chatusers.id FROM tokens,chatusers WHERE tokens.user=chatusers.id;"
+            cursor.execute(query)
+            if cursor.rowcount == 0:
+                return None
+            return cursor.fetchone()[0]
+        return None

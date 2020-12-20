4fe75952a41a37fecdf0620ae26814c54be030cc
imskGithub <65082478+imskGithub@users.noreply.github.com>
2020-12-20 18:22:24 +0000

    Class Toggle fixed

--- UI/js/index.js
+++ UI/js/index.js
@@ -1,6 +1,30 @@
-function anim() {
-    document.getElementById('middleBox').classList.add('middle-box-anim');
-    document.getElementById('logoId').classList.add('logo-anim');
-    document.getElementById('animbox').style.opacity = "0"; 
-    document.getElementById('form').style.opacity = "1";       
+// function signIN() {
+//     document.getElementById('middleBox').classList.add('middle-box-anim');
+//     document.getElementById('logoId').classList.add('logo-anim');
+//     document.getElementById('animbox').style.opacity = "0";
+//     document.getElementById('form').style.opacity = "1";   
+    
+// }
+
+function signIN() {
+    document.getElementById('middleBox').classList.toggle('middle-box-anim');
+    document.getElementById('logoId').classList.toggle('logo-anim');
+    document.getElementById('animbox').style.opacity = "0";
+    document.getElementById('form').style.opacity = "1";
+    document.getElementById('form').style.transitionDelay = "1s";
+    document.getElementById('animbox').style.transitionDelay = "0ms"; 
+    document.getElementById('animbox').style.pointerEvents = "none";   
+    document.getElementById('form').style.pointerEvents = "auto";   
+    
+}
+
+function notSignIN() {
+    document.getElementById('middleBox').classList.toggle('middle-box-anim');
+    document.getElementById('logoId').classList.toggle('logo-anim');
+    document.getElementById('animbox').style.opacity = "1";
+    document.getElementById('form').style.opacity = "0";
+    document.getElementById('form').style.transitionDelay = "0ms";
+    document.getElementById('animbox').style.transitionDelay = "0.7s";  
+    document.getElementById('form').style.pointerEvents = "none";
+    document.getElementById('animbox').style.pointerEvents = "auto";   
 }

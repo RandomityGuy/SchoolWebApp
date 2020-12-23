df6b0fbaf42995f1ff9d8f94de8dbbba695d7235
imsk <kyrielityryu@gmail.com>
2020-12-23 22:41:26 +0530

    added channels

--- UI/js/chat.js
+++ UI/js/chat.js
@@ -2,3 +2,11 @@ function toggleUserDetails() {
   document.getElementById('container').classList.toggle('disable');
   document.getElementById('popup-user-detail').classList.toggle('hide');
 }
+
+function showBorder(element) {
+  element.classList.add('border');
+}
+
+function hideBorder(element) {
+  element.classList.remove('border');
+}

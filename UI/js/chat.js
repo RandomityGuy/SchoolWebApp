9685a920c4cee57312f46a8adc22726b89520ac8
RandomityGuy <31925790+RandomityGuy@users.noreply.github.com>
2020-12-24 10:52:57 +0530

    Get basic channel viewing done

--- UI/js/chat.js
+++ UI/js/chat.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (function () {
   'use strict';
 
@@ -10,6 +9,7 @@
   const panel_profile = document.querySelector("#main-panel-profile");
   const popup_details_back = document.querySelector("#popup-user-detail-back");
   const main_panel_messages = document.querySelector("#main-panel-messages");
+  const channel_list = document.querySelector("#channels");
   class Channel {
       constructor(id, name, flags) {
           this.id = id;
@@ -33,8 +33,19 @@
   id = parseInt(localStorage.getItem('id'));
   fetch("/api/channels?" + new URLSearchParams({ token: token.toString() })).then(response => response.json()).then(data => {
       channels = [];
+      let idx = 0;
       data.channels.forEach(element => {
           channels.push(new Channel(element.id, element.name, element.flags));
+          let channel_div = document.createElement('div');
+          channel_div.className = "channel";
+          channel_div.id = idx.toString();
+          channel_div.dataset.id = element.id;
+          channel_div.addEventListener('mouseover', (e) => showBorder(channel_div));
+          channel_div.addEventListener('mouseout', (e) => hideBorder(channel_div));
+          channel_div.addEventListener('click', (e) => set_channel(parseInt(channel_div.id)));
+          channel_div.innerHTML = element.name;
+          channel_list.appendChild(channel_div);
+          idx++;
       });
       current_channel = 0;
       set_channel_data();
@@ -49,6 +60,11 @@
   function toggleUserDetails() {
       document.getElementById('container').classList.toggle('disable');
       document.getElementById('popup-user-detail').classList.toggle('hide');
+  }
+  function set_channel(channel_index) {
+      current_channel = channel_index;
+      set_channel_data();
+      get_chat();
   }
   function set_channel_data() {
       let channel = channels[current_channel];
@@ -75,6 +91,7 @@
   }
   function get_chat() {
       let channel = channels[current_channel];
+      main_panel_messages.innerHTML = "";
       fetch(`/api/channels/${channel.id}/messages?` + new URLSearchParams({ token: token.toString() })).then(response => response.json()).then(data => {
           data.messages.forEach(element => {
               let sender_name = element.author.name;
@@ -99,6 +116,12 @@
               main_panel_messages.appendChild(div);
           });
       });
+  }
+  function showBorder(element) {
+      element.classList.add('border');
+  }
+  function hideBorder(element) {
+      element.classList.remove('border');
   }
 
 }());
